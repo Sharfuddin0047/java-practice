5699ce076af88106e901b5d58db7a32a229beb3c
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-22 21:52:23 +0530

    BitManipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -27,6 +27,30 @@ public class BitManipulation {
         int bitMask =~(1<<i);
         return n & bitMask;
     }
+
+    public static int countSetbits(int n){
+        int count = 0;
+        while(n>0){
+            if((n & 1) != 0){
+                count ++;
+            }
+            n = n>>1;
+        }
+        return count;
+    }
+
+    public static int fastExpo(int a,int n){
+        int ans = 1;
+
+        while(n>0){
+            if((n & 1) != 0){
+                ans = ans *a;
+            }
+            a = a*a;
+            n = n>>1;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         // int n=5;
         // oddOrEven(n);
@@ -38,7 +62,14 @@ public class BitManipulation {
         // System.out.println(setithBit(10, 2));
 
 
-        System.out.println(clearithBit(10, 3));
+        // System.out.println(clearithBit(10, 3 ));
+
+
+
+        // System.out.println(countSetbits(11));
+
+
+        System.out.println(fastExpo(5,3));
         
     }
     
