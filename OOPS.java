83c4b60764153116e410c35664b1ab0119ad0059
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-26 21:16:37 +0530

    getter-setter

--- OOPS.java
+++ OOPS.java
@@ -2,50 +2,35 @@ public class OOPS {
     public static void main(String[] args) {
         pen p1 = new pen(); //object p1
         p1.setColor("Blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.setTip(5);
-        System.out.println(p1.tip);
+        System.out.println(p1.getTip());
         p1.setColor("Yellow");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
 
 
-        BankAccount myAcc = new BankAccount();
-        myAcc.username = "abc";
-        myAcc.setPassword("xyz");
-        // System.out.println(myAcc.username);
+       
         
 
     }
     
 }
-class BankAccount {
-    public String username;
-    private String password;
-    public void setPassword(String pwd){
-        password = pwd;
-        // System.out.println(password);
-    }
 
-}
 class pen {
-    String color;
-    int tip;
-
+    private String color;
+    private int tip;
+    int getTip(){
+        return this.tip;
+    }
+    String getColor(){
+        return this.color;
+    }
     void setColor(String newColor){
-        color = newColor;
+        this.color = newColor;
     }
 
     void setTip(int newTip){
-        tip = newTip;
+        this.tip = newTip;
     }
-    class Student{
-        String name;
-        int age;
-        float percentage;
-
-        void calcPercentage(int phy, int chem,int math){
-            percentage = (phy + chem + math)/3;
-
-        }
-    }
+    
 }
