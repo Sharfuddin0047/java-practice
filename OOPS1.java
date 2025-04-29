42dd49382fa73602071c0f83970961b18922edcd
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-29 21:58:56 +0530

    abstraction

--- OOPS1.java
+++ OOPS1.java
@@ -9,8 +9,19 @@ public class OOPS1 {
 
 
         // Method Overriding
-        Deer d1 =new Deer();
-        d1.eats();
+        // Deer d1 =new Deer();
+        // d1.eats();
+
+        // abstract 
+        // Horse h = new Horse();
+        // h.eat();
+        // h.walks();
+
+        // Chicken c = new Chicken();
+        // h.eat();
+        // h.walks();
+
+        Mustang myHorse = new Mustang();
         
     }
     
@@ -33,14 +44,64 @@ public class OOPS1 {
 
 
 /*Method overriding : Parent and child classes both contain the same function with a different definition */
-class Animal{
-    void eats(){
-        System.out.println("Eats anything");
+// class Animal{
+//     void eats(){
+//         System.out.println("Eats anything");
+//     }
+// }
+
+// class Deer extends Animal{
+//     void eats(){
+//         System.out.println("Eats grass");
+//     }
+// }
+
+
+
+
+
+/*Abstract class
+ * Cannot create an instance of abstract class
+ * can have abstract/non-abstract methods
+ * can have constructors
+  */
+
+  abstract class Animal{
+    String color;
+    Animal(){
+        System.out.println("Animal constructor called....");
+    }
+
+    void eat(){
+        System.out.println("Animal eats");
+    }
+    abstract void walks();
+
+  }
+
+  class Horse extends Animal{
+    Horse(){
+        System.out.println("Horse constructor called... ");
+    }
+    void changeColor(String color){
+        color = "dark brown";
+    }
+    void walks(){
+        System.out.println("walks on 4 legs");
     }
 }
 
-class Deer extends Animal{
-    void eats(){
-        System.out.println("Eats grass");
+class Mustang extends Horse{
+    Mustang(){
+        System.out.println("Mustang constructor called....");
+    }
+}
+
+class Chicken extends Animal{
+    void changeColor(){
+        color = " yellow";
+    }
+    void walks(){
+        System.out.println("Walks on 2 legs");
     }
 }
