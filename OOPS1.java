3a53a38b1dd26d70b0848f3b396d8d75b459b32b
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-29 20:54:48 +0530

    Method overriding

--- OOPS1.java
+++ OOPS1.java
@@ -1,23 +1,46 @@
 public class OOPS1 {
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println(cal.sum(2,4));
-        System.out.println(cal.sum(2.4f,3.4f));
-        System.out.println(cal.sum(1,2,3));
+
+        // method overloading
+        // Calculator cal = new Calculator();
+        // System.out.println(cal.sum(2,4));
+        // System.out.println(cal.sum(2.4f,3.4f));
+        // System.out.println(cal.sum(1,2,3));
+
+
+        // Method Overriding
+        Deer d1 =new Deer();
+        d1.eats();
         
     }
     
 }
 
-class Calculator{
-    int sum(int a,int b){
-        return a+b;
-    }
-    float sum(float a, float b){
-        return a+b;
 
+// method overloading
+// class Calculator{
+//     int sum(int a,int b){
+//         return a+b;
+//     }
+//     float sum(float a, float b){
+//         return a+b;
+
+//     }
+//     int sum(int a, int b ,int c){
+//         return a+b+c;
+//     }
+// }
+
+
+/*Method overriding : Parent and child classes both contain the same function with a different definition */
+class Animal{
+    void eats(){
+        System.out.println("Eats anything");
     }
-    int sum(int a, int b ,int c){
-        return a+b+c;
+}
+
+class Deer extends Animal{
+    void eats(){
+        System.out.println("Eats grass");
     }
 }
