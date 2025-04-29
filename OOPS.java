e62a3ae9fc1fe73caecbc30a32b6afb16198f4fb
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-29 08:33:27 +0530

    deep copy constructor

--- OOPS.java
+++ OOPS.java
@@ -34,12 +34,26 @@ class Student{
     int roll;
     String password; 
     int marks[];
-    Student(Student s1){
+
+    //Shallow copy consttructor
+    // Student(Student s1){
+    //     marks = new int[3];
+    //     this.name = s1.name;
+    //     this.roll = s1.roll;
+    //     this.marks = s1.marks;
+    // }
+
+    // deep copy constructor
+    Student( Student s1){
         marks = new int[3];
         this.name = s1.name;
         this.roll = s1.roll;
-        this.marks = s1.marks;
+        for(int i=0; i<marks.length; i++){
+            this.marks[i] = s1.marks[i];
+        }
     }
+
+
 
     Student(){
         System.out.println("constructor is called...");
