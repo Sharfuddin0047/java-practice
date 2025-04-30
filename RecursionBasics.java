794773a502dadf2501babdf68ccadbcc1977ccfd
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-30 22:05:06 +0530

    factorial,sum

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -1,28 +1,50 @@
 public class RecursionBasics {
-    public static void printDec(int n){
-        if(n==1){
-            System.out.println(n);
-            return;
-        }
-        System.out.print(n+" ");
-        printDec(n-1);
-    }
+    // public static void printDec(int n){
+    //     if(n==1){
+    //         System.out.println(n);
+    //         return;
+    //     }
+    //     System.out.print(n+" ");
+    //     printDec(n-1);
+    // }
 
-    public static void printInc(int n){
+    // public static void printInc(int n){
+    //     if(n==1){
+    //         System.out.print(n+" ");
+    //         return;
+    //     }
+    //     printInc(n-1);
+    //     System.out.print(n+" ");
+    // }
+
+
+
+    // public static int fact(int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     int fn = n*fact(n-1);
+    //     return fn;
+    // }
+
+      public static int calcSum(int n){
         if(n==1){
-            System.out.print(n+" ");
-            return;
+            return 1;
         }
-        printInc(n-1);
-        System.out.print(n+" ");
-    }
+        int sum = n + calcSum(n-1);
+        return sum;
+      }
+
 
     public static void main(String[] args) {
-        int n = 10;
-        printDec(n);
+        // int n = 10;
+        // printDec(n);
+        // printInc(n);
 
 
-        printInc(n);
+       // System.out.println(fact(5));
+
+       System.out.println(calcSum(5));
     }
     
 }
