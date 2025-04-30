ab242bfceea048ec88bb89da375cd93a80a0c34c
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-04-30 21:38:53 +0530

    printInc

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -8,9 +8,21 @@ public class RecursionBasics {
         printDec(n-1);
     }
 
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
+
     public static void main(String[] args) {
         int n = 10;
         printDec(n);
+
+
+        printInc(n);
     }
     
 }
