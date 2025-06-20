555b9902666dc8200e0b42744d36114ab2a8eafe
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-06-20 23:23:09 +0530

    sorted rotated search

--- DividenConquer.java
+++ DividenConquer.java
@@ -1,45 +1,46 @@
 public class DividenConquer {
-    public static void printArr(int arr[]){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]+" ");
+    public static int search(int arr[], int tar, int si, int ei){
+        if(si > ei){
+            return -1;
         }
-        System.out.println();
-    }
-    public static void quickSort(int arr[], int si, int ei){
-        if(si >= ei){
-            return;
+        //kaam
+        int mid =(si+ei)/2;
+
+        //case FOUND
+        if(arr[mid] == tar){
+            return mid;
         }
-        //last element
-        int pIdx = partition(arr,si,ei);
-        quickSort(arr, si, pIdx-1); //left
-        quickSort(arr, pIdx+1, ei); //right
 
-    }
-
-    public static int partition(int arr[], int si, int ei){
-        int pivot = arr[ei];
-        int i = si-1; // to make place elements smaller than pivot
-
-        for(int j=si; j<ei; j++){
-            if(arr[j] <= pivot){
-                i++;
-                //swap
-                int temp = arr[j];
-                arr[j] = arr[i];
-                arr[i] = temp;
+        //mid on L1
+        if(arr[si] <= arr[mid]){
+            //case a:left
+            if(arr[si] <= tar && tar <= arr[mid]){
+                return search(arr, tar, si, mid-1);
+            }else{
+                //caseb: right
+                return search(arr, tar, mid+1, ei);
             }
-        }
-        i++;
-        int temp = pivot;
-        arr[ei] = arr[i];
-        arr[i] = temp;
-        return i;
 
+        }
+        
+        //mid on L2
+        else{
+            //case c: right
+            if(arr[mid] <= tar && tar <= arr[ei]){
+                return search(arr, tar, mid+1, ei);
+            }else{
+                //case d: left
+                return search(arr, tar, si, mid-1);
+            }
+
+        }
     }
+    
     public static void main(String[] args) {
-        int arr[] = {6, 3, 9, 8, 2, 5};
-        quickSort(arr,0,arr.length-1 );
-        printArr(arr);
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        int tarIdx = search(arr, target, 0, arr.length-1);
+        System.out.println(tarIdx);
         
     }
     
