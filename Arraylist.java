357ffeaf7e9d64e7da2b4bf4f050f4b11daedc7d
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-07-29 08:34:00 +0530

    swap

--- Arraylist.java
+++ Arraylist.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 
 public class Arraylist {
+    public static void swap(ArrayList<Integer> list, int idx1, int idx2){
+        //swap two numbers
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2,temp);
+    }
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         ArrayList<String> list2 = new ArrayList<>();
@@ -14,6 +20,9 @@ public class Arraylist {
 
         list.add(1,9);
 
+        int idx1 = 1, idx2 = 3;
+        System.out.println(list);
+        swap(list,idx1,idx2);
         System.out.println(list);
 
         //Get operation
@@ -33,28 +42,28 @@ public class Arraylist {
         // System.out.println(list.contains(1));
         // System.out.println(list.contains(11));
 
-        System.out.println(list.size());
+        //System.out.println(list.size());
 
         //print the arraylist
-        for(int i=0; i<list.size(); i++){
-            System.out.print(list.get(i)+ " ");
+        // for(int i=0; i<list.size(); i++){
+        //     System.out.print(list.get(i)+ " ");
 
-        }
-        System.out.println();
+        // }
+        // System.out.println();
 
         // Reverse print
-        for(int i=list.size()-1; i>=0; i--){
-            System.out.print(list.get(i)+ " ");
-        }
-        System.out.println();
+        // for(int i=list.size()-1; i>=0; i--){
+        //     System.out.print(list.get(i)+ " ");
+        // }
+        // System.out.println();
 
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<list.size(); i++){
-            // if(max<list.get(i)){
-            //     max = list.get(i);
-            // }
-            max = Math.max(max,list.get(i));
-        }
-        System.out.println("max is : "+max);
+        // int max = Integer.MIN_VALUE;
+        // for(int i=0; i<list.size(); i++){
+        //     // if(max<list.get(i)){
+        //     //     max = list.get(i);
+        //     // }
+        //     max = Math.max(max,list.get(i));
+        // }
+        // System.out.println("max is : "+max);
     }
 }
