6aafc48ba17e5f527b0e38680f95b95404cfbdb0
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-07-29 16:02:39 +0530

    pointer usage

--- Container.java
+++ Container.java
@@ -3,18 +3,43 @@
 import java.util.ArrayList;
 
 public class Container {
+    // public static int storeWater(ArrayList<Integer> height){
+    //     int maxWater = 0;
+    //     //brute force
+    //     for(int i=0; i<height.size(); i++){
+    //         for(int j=i+1; j<height.size(); j++){
+    //             int ht = Math.min(height.get(i), height.get(j));
+    //             int width = j-1;
+    //             int currWater = ht * width;
+    //             maxWater = Math.max(maxWater, currWater);
+    //         }
+    //     }
+
+    //     return maxWater;
+    // }
+
+    // Two pointer approach
+
     public static int storeWater(ArrayList<Integer> height){
         int maxWater = 0;
-        //brute force
-        for(int i=0; i<height.size(); i++){
-            for(int j=i+1; j<height.size(); j++){
-                int ht = Math.min(height.get(i), height.get(j));
-                int width = j-1;
-                int currWater = ht * width;
-                maxWater = Math.max(maxWater, currWater);
-            }
-        }
+        int lp = 0;
+        int rp = height.size()-1;
 
+        while(lp<rp){
+            //calculate area 
+            int ht =Math.min(height.get(lp), height.get(rp));
+            int width = rp -lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            //update ptr
+            if(height.get(lp) < height.get(rp)){
+                lp++;
+            }else{
+                rp--;
+            }
+
+        }
         return maxWater;
     }
     public static void main(String[] args) {
