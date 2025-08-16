f90a98dface46a0ec4c0a0da91aa1e1383323f4f
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-08-16 15:32:51 +0530

    zigZag LinkedList

--- Linked_List/LinkedList.java
+++ Linked_List/LinkedList.java
@@ -308,6 +308,45 @@ public class LinkedList {
         return merge(newLeft, newRight);
     }
 
+    public void zigZag() {
+        //find mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        //reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        //alt merge - zig-zag merge
+        while(left != null && right != null){
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         // ll.print();
@@ -335,18 +374,28 @@ public class LinkedList {
         // ll.deleteNthfromEnd(3);
         // ll.print();
 
-        ll.addLast(1);
+        // ll.addLast(1);
 
-        ll.addLast(2);
+        // ll.addLast(2);
 
-        ll.addLast(2);
-        ll.addLast(1);
+        // ll.addLast(2);
+        // ll.addLast(1);
 
-        ll.print();
-        //System.out.println(ll.checkPalindrome());
+        // ll.print();
+        // //System.out.println(ll.checkPalindrome());
          
-        //System.out.println(ll.isCycle());
-        ll.head = ll.mergeSort(ll.head);
+        // //System.out.println(ll.isCycle());
+        // ll.head = ll.mergeSort(ll.head);
+        // ll.print();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        //1->2->3->4->5->null
+        ll.print();
+        ll.zigZag();
         ll.print();
 
     }
