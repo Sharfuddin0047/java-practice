0f171d67b7af5e2b03e527001fd23c3499cc63e9
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-08-19 13:44:44 +0530

    Reverse a string

--- stacks/Bottompush.java
+++ stacks/Bottompush.java
@@ -9,17 +9,35 @@ public class Bottompush {
         int top = s.pop();
         pushAtBottom(s, data);
         s.push(top);
+    } 
+    public static String reverseString(String str) {
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx<str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
     }
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        pushAtBottom(s, 4);
-        while (!s.isEmpty()) {
-            System.out.println(s.pop());
-        }
+        // pushAtBottom(s, 4);
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.pop());
+        // }
+
+
+        String str = "abc";
+        System.out.println(reverseString(str));
     }
     
 }
