ef7133ad22edbb4ed2242a1fa39fdfd03085ca9d
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-08-20 17:08:22 +0530

    Duplicate Parenthesis

---
+++ stacks/DuplicateParenthesis.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class DuplicateParenthesis {
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+
+        for(int i =0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            if(ch == ')'){
+                int counter = 0;
+                while(s.peek() != '(') {
+                    s.pop();
+                    counter++;
+                }
+                if(counter < 1) {
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }
+            else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        //Valid String 
+        String str = "((a+b))";
+        String str1 = "(a-b)";
+        System.out.println(isDuplicate(str));
+        System.out.println(isDuplicate(str1));
+    }
+}
