2f9bfb6ae268a0f2a15ee3d846cda980e15f2c3a
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-08-23 15:53:46 +0530

    circular queue using arrays

--- queue/QueueA.java
+++ queue/QueueA.java
@@ -4,24 +4,35 @@ public class QueueA {
         static int arr[];
         static int size;
         static int rear;
+        static int front;
+
         Queue(int n) {
             arr = new int[n];
             size = n;
             rear = -1;
+            front = -1;
         }
 
         public static boolean isEmpty() {
-            return rear == -1;
+            return rear == -1 && front == -1;
         }
+
+        public static boolean isFull() {
+            return (rear+1)%size == front;
+        }
+
 
         //add
         public static void add(int data) {
-            if(rear == size -1) {
+            if(isFull()) {
                 System.out.println("queue is full");
                 return;
             }
-
-            rear = rear + 1;
+            //add first element
+            if(front == -1) {
+                front = 0; 
+            }
+            rear = (rear + 1) % size;
             arr[rear] = data;
         }
 
@@ -31,12 +42,15 @@ public class QueueA {
                 System.out.println("queue is empty");
                 return -1;
             }
-            int front = arr[0];
-            for(int i=0; i<rear; i++){
-                arr[i] = arr[i+1];
+            int result = arr[front]; 
+            
+            //last element delete
+            if(rear == front) {
+                rear = front = -1;
+            }else{
+                front = (front+1) % size;
             }
-            rear = rear - 1;
-            return front;
+            return result;
         }
 
         //peek
@@ -45,7 +59,7 @@ public class QueueA {
                 System.out.println("queue is empty");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
     public static void main(String[] args) {
@@ -53,7 +67,10 @@ public class QueueA {
         q.add(1);
         q.add(2);
         q.add(3);
-
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
         while(!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
