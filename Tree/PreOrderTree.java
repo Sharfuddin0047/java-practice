ca3140e4c9a5a153d3ee6d901b1b72cc6d5742e4
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-09-02 09:32:33 +0530

    inorder traversal

--- Tree/PreOrderTree.java
+++ Tree/PreOrderTree.java
@@ -33,6 +33,15 @@ public class PreOrderTree {
             preorder(root.left);
             preorder(root.right);
         }
+
+        public static void inorder(Node root) {
+            if(root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+ " ");
+            inorder(root.right);
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -41,5 +50,7 @@ public class PreOrderTree {
         System.out.println(root.data);
 
         tree.preorder(root);
+        System.out.println();
+        tree.inorder(root);
     }
 }
