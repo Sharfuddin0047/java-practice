1e054003281a9c0704c1d4e796a83935ec5783c6
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-09-03 18:39:27 +0530

    sum of nodes data

--- Tree/HeightTree.java
+++ Tree/HeightTree.java
@@ -28,6 +28,16 @@ public class HeightTree {
             int rc = count(root.right);
             return lc + rc +1;
         }
+
+        public static int sum(Node root) {
+            if(root == null) {
+                return 0;
+            }
+
+            int ls = sum(root.left);
+            int rs = sum(root.right);
+            return ls + rs + root.data;
+        }
     public static void main(String[] args){
         /*
          *         1
@@ -42,10 +52,11 @@ public class HeightTree {
          root.right = new Node(3);
          root.left.left = new Node(4);
          root.left.right = new Node(5);
-         root.left.right = new Node(6);
+         root.right.left = new Node(6);
          root.right.right = new Node(7);
 
-         System.out.println(height(root));
-         System.out.println(count(root));
+         //System.out.println(height(root));
+         //System.out.println(count(root));
+         System.out.println(sum(root));
     }
 }
