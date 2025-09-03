b73086dfa112481cc58f6d98f1a15030bf75aaa8
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-09-03 16:14:12 +0530

    level order traversal

--- Tree/PreOrderTree.java
+++ Tree/PreOrderTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class PreOrderTree {
     static class Node {
         int data;
@@ -42,6 +44,36 @@ public class PreOrderTree {
             System.out.print(root.data+ " ");
             inorder(root.right);
         }
+
+        //Level ordr traversal
+        public static void levelOrder(Node root) {
+            if(root == null) {
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()) {
+                Node currNode = q.remove();
+                if(currNode == null) {
+                    System.out.println();
+                    if(q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data+ " ");
+                    if(currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if(currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
@@ -52,5 +84,7 @@ public class PreOrderTree {
         tree.preorder(root);
         System.out.println();
         tree.inorder(root);
+        System.out.println();
+        tree.levelOrder(root);
     }
 }
