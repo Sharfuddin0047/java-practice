412beb8b96f655dfacf3de68a176df2de32b3467
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-09-06 15:15:57 +0530

    Approach 2 for diameter

--- Tree/HeightTree.java
+++ Tree/HeightTree.java
@@ -39,19 +39,41 @@ public class HeightTree {
             return ls + rs + root.data;
         }
 
-        public static int diameter(Node root) { //O(n^2)
+        public static int diameter2(Node root) { //O(n^2)
             if(root == null) {
                 return 0;
             }
 
-            int leftDiam = diameter(root.left);
+            int leftDiam = diameter2(root.left);
             int leftHt = height(root.left);
-            int rightDiam = diameter(root.right);
+            int rightDiam = diameter2(root.right);
             int rightHt = height(root.right);
 
             int selfDiam = leftHt + rightHt + 1;
 
             return Math.max(selfDiam, Math.max(leftDiam, rightDiam));
+        }
+
+        static class Info{
+            int diam;
+            int ht;
+
+            public Info(int diam, int ht) {
+                this.diam = diam;
+                this.ht = ht;
+            }
+        }
+        public static Info diameter(Node root) {
+            if(root == null){
+                return new Info(0, 0);
+            }
+            Info leftInfo = diameter(root.left);
+            Info rightInfo = diameter(root.right);
+
+            int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+            int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+
+            return new Info(diam, ht);
         }
     public static void main(String[] args){
         /*
@@ -73,6 +95,7 @@ public class HeightTree {
          //System.out.println(height(root));
          //System.out.println(count(root));
         //System.out.println(sum(root));
-        System.out.println(diameter(root));
+        // System.out.println(diameter2(root));
+        System.out.println(diameter(root).diam);
     }
 }
