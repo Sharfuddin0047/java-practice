338414f500be11f141fc7a97fa7e9bb250a56831
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-10-05 22:42:57 +0530

    sum tree

--- Tree/KthLevel.java
+++ Tree/KthLevel.java
@@ -136,6 +136,29 @@ public class KthLevel {
             } 
         return max +1;
     }
+    public static int transform(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+        
+        int data = root.data;
+
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild + newRight + rightChild;
+        return data;
+    }
+
+    public static void preorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        System.out.println(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     
     public static void main(String[] args) {
         Node root = new Node(1);
@@ -155,7 +178,9 @@ public class KthLevel {
         // int n1 = 4, n2 = 6;
         // System.out.println(minDst(root, n1, n2));
 
-        int n=5, k=2;
-        KAncestor(root, n, k);
+        // int n=5, k=2;
+        // KAncestor(root, n, k);
+        transform(root);
+        preorder(root);
     }
 }
