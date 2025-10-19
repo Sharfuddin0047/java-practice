f55fef028239fdac537a2caba6c88f262b067ade
Sharfuddin0047 <sharfuddin.flamingo@gmail.com>
2025-10-19 21:05:51 +0530

    PQ in object

--- Heap/PQinJcf.java
+++ Heap/PQinJcf.java
@@ -1,18 +1,34 @@
 package Heap;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PQinJcf {
-    public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static class Student implements Comparable<Student> { //overriding
+        String name;
+        int rank;
 
-        pq.add(3);//O(logn)
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(new Student("A", 4));//O(logn)
+        pq.add(new Student("B", 5));//O(logn)
+        pq.add(new Student("C", 2));//O(logn)
+        pq.add(new Student("D", 12));//O(logn)
+        
 
         while(!pq.isEmpty()) {
-            System.out.println(pq.peek()); //O(1)
+            System.out.println(pq.peek().name + "->"+pq.peek().rank); //O(1)
             pq.remove(); //O(logn)
         }
     }
